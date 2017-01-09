58349dada2363ca1ee6a6b12c864e8a816ee7489
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-01-09 13:57:18 -0500

    stopped page reload on form submit

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -6,59 +6,81 @@
 function Search() {
 
 	$('#txtDept').selectize({
-        valueField: 'value',
+        valueField: 'dept_id',
         labelField: 'dept_id',
         searchField: 'dept_id',
         sortField: 'dept_id',
         selectOnTab: true,
         closeAfterSelect: true,
-        loadThrottle: 50,
-        render: {
-            option: function(item, escape) {
-                return '<div>' + escape(item.dept_id) + '</div>';;
-            }
-        },
-        load: function(query, callback) {
-            $.ajax({
-                url: API_DEPARTMENTS,
-                type: 'GET',
-                error: function() {
-                    callback();
-                },
-                success: function(res) {
-                    for (var i = 0; i < res.length; i++) {
-                        res[i]['value'] = res[i].dept_id;
-                    }
-                    callback(res);
-                }
-            });
+        onInitialize: function() {
+       		var self = this;
+       		$.get(API_DEPARTMENTS, function(data) {
+       			self.addOption(data);
+       		})
         }
     });
 
     $('#txtGened').selectize({
-    	sortField: 'text'
-    })[0].selectize.clear();
-
-    
-
-
-	var query = window.location.href.split('?')[1];
-
-	if (query) {
-		this.processQuery('?' + query);
-	} else {
-		$('.data-loading').hide();
-		$('.empty-data').show();
-	}
+    	sortField: 'text',
+    	onInitialize: function() {
+    		this.clear();
+    	}
+    });
 
 
+    var self = this;
+    $('#btnReset').click(function() {
+    	self.resetForm();
+    })
+
+    // don't reload page, just process here
+    $('form').submit(function(e) {
+    	e.preventDefault();
+
+    	$('.search-results').empty();
+
+    	var query = self.buildQuery();
+		self.processQuery('?' + query);
+
+    })
 
 }
 
+// clear all input fields
+Search.prototype.resetForm = function() {
+	$('.form-group > input').each(function(index) {
+		$(this).val('');
+	});
+
+	$('.form-group > select').each(function(index) {
+		$(this).selectize()[0].selectize.clear(false);
+	})
+}
+
+// build querystring from input fields in search form
+Search.prototype.buildQuery = function() {
+	var params = {};
+
+	$('.form-group > input').each(function(index) {
+		params[$(this).attr('name')] = $(this).val()
+	});
+
+	$('.form-group > select').each(function(index) {
+		params[$(this).attr('name')] = $(this).val();
+	});
+
+
+	return $.param(params);
+}
+
+// use query to search for courses
 Search.prototype.processQuery = function(query) {
 	console.log('processing query');
 
-	var $resultsWrap = $('.search-results-wrap');
+	$('.empty-data').hide();
+	$('.data-loading').show();
+
+	var $resultsWrap = $('.search-results');
 
 	// add semester array to node DB, allow lookup on umd.io 
 	$.get(API_FIND_COURSES + query, function(data) {
@@ -107,13 +129,24 @@ function generateSearchItem(course) {
 		description: course.description
 	};
 	
-
 	var body = template(data);
 
-	//var $a = $('<a/>', {'href': url});
-	//$a.html(body);
-
 	return body;
+}
+
+function saveFormData() {
+	var storage = window.localStorage;
+	storage.setItem('keyword', $('#txtKeyword').val());
+	storage.setItem('dept', $('#txtDept').val());
+	storage.setItem('gened', $('#txtGened').val());
+}
+
+function loadFormData() {
+	var storage = window.localStorage;
+	$('#txtKeyword').val(storage.getItem('keyword'));
+	$('#txtDept').selectize()[0].selectize.addItem({dept_id: 'CMSC'});
+	$('#txtDept').val(storage.getItem('dept')); // fix
+	$('#txtGened').val(storage.getItem('gened')); // fix
 }
 
 
