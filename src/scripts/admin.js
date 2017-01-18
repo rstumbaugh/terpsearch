9640289824471c2783386abe0fcbdd3a70737b1a
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-01-18 00:03:49 -0500

    worked on dashboard, updated footers

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -3,6 +3,17 @@ function Admin() {
 	
 	this.auth = firebase.auth();
 
+	$('.tab').click(function() {
+		var id = $(this).attr('id');
+		console.log(id);
+
+		$('.tab.active').removeClass('active');
+		$(this).addClass('active');
+
+		$('.content-wrap .content').hide();
+		$('.content-wrap .content.'+id).slideDown();
+	})
+
 	var self = this;
 	this.loginAndValidate().then(function(data) {
 		
@@ -10,11 +21,46 @@ function Admin() {
 		$('#name').text(data.name);
 		$('.logged-in').slideDown();
 
+		$('.content-wrap .content').hide();
+		$('.content.logs').show();
+
+		self.loadLogs(data.logs);
 		self.loadEmails(data.emails);
 
 	}).catch(function(err) {
 		console.log(err);
 	});
+}
+
+Admin.prototype.loadLogs = function(logs) {
+	console.log('found '+logs.length+' logs');
+
+	if (logs.length > 0) {
+		$('.logs .empty').hide();
+		$('.logs .table').removeClass('hidden');
+	}
+
+	var $table = $('.logs .table');
+	for (var i = 0; i < logs.length; i++) {
+		var log = logs[i];
+		var content = log.content;
+
+		if (log.type == 'rating') {
+			content = content.course_id + ': Difficulty = '+content.difficulty+', Interest = '
+						+content.interest+' ('+content.professor+')';
+		} else if (log.type == 'comment') {
+			content = content.course_id + ': '+content.comment;
+		}
+
+		var $row = $('<tr/>');
+		var $type = $('<td/>').text(log.type);
+		var $content = $('<td/>').text(content);
+		var $time = $('<td/>').text(new Date(log.time).toString('hh:mm tt MMM dd yyyy'));
+
+		$row.append($type).append($content).append($time);
+
+		$table.append($row);
+	}
 }
 
 Admin.prototype.loadEmails = function(emails) {
@@ -26,7 +72,6 @@ Admin.prototype.loadEmails = function(emails) {
 	var $table = $('.email .table');
 
 	for (var i = 0; i < emails.length; i++) {
-		
 		var email = emails[i];
 
 		var $row = $('<tr/>');
@@ -48,7 +93,7 @@ Admin.prototype.loginAndValidate = function() {
 			var token = result.credential.accessToken;
 			var user = result.user;
 			var name = user.displayName.split(' ')[0];
-
+			var name = 'Ryan';
 			user.getToken(true).then(function(token) {
 
 				var p = $.get(API_ROOT + 'admin/dash?token='+token);
@@ -58,9 +103,7 @@ Admin.prototype.loginAndValidate = function() {
 				}).fail(function(xhr, status, err) {
 					reject('not authorized.');
 				})
-			})
-		}).catch(function(error) {
-			reject('Unable to log in (check your popup settings?)');
+			 })
 		});
 	});
 }
