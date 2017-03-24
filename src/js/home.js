92bb0c10b2860bd1d65505c678c0bf4880cebef7
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-03-24 18:43:50 -0400

    redesigned home page

--- src/js/home.js
+++ src/js/home.js
@@ -7,80 +7,6 @@ var globals = require('./modules/globals.js');
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-var EmailBox = React.createClass({
-	getInitialState: function() {
-		return {
-			email: '',
-			message: '',
-			slideClass: 'message-wrap slide closed',
-			test: 'red'
-		}
-	},
-	handleChange: function(e) {
-		this.setState({
-			email: e.target.value,
-			message: '',
-			slideClass: 'slide closed'
-		});
-	},
-	handleSubmit: function() {
-		
-		if (this.state.email.match(globals.EMAIL_REGEX)) {
-			var self = this;
-
-			fetch(globals.API_ADD_EMAIL, {
-				method: 'post',
-				headers: {
-			    	'Accept': 'application/json',
-			    	'Content-Type': 'application/json'
-			    },
-				body: JSON.stringify({ 'email': this.state.email })
-			})
-			.then(globals.handleFetchResponse)
-		    .then(function(response) {
-				self.setState({
-					email: '',
-					message: 'Thanks!',
-					slideClass: 'slide open'
-				})
-			})
-			.catch(function(err) {
-				self.rejectEmail();
-			})
-		} else {
-			this.rejectEmail();
-		}
-		
-	},
-	rejectEmail: function() {
-		this.setState({
-			message: 'Please enter a valid email.',
-			slideClass: 'slide open'
-		});
-	},
-	render: function() {
-		return (
-			<div>
-				<div className="input-group">
-					<input type="text" 
-						   className="form-control" 
-						   placeholder="me@example.com" 
-						   value={this.state.email}
-						   onChange={this.handleChange}>
-					</input>
-		            <span className="input-group-btn">
-		            	<button className="btn btn-default" onClick={this.handleSubmit}>Go!</button>
-		        	</span>
-		        </div>
-		        <br/>
-	            <div className={'message-wrap '+this.state.slideClass}>
-	            	{this.state.message}
-	            </div>
-	        </div>
-		)
-	}
-});
-
 var Feedback = React.createClass({
 	getInitialState: function() {
 		return {
@@ -149,7 +75,6 @@ var Feedback = React.createClass({
 	}
 })
 
-ReactDOM.render(<EmailBox />, document.getElementById('email'));
 ReactDOM.render(<Feedback />, document.getElementById('feedback'));
 ReactDOM.render(<RatingForm />, document.getElementById('ratingForm'));
 ReactDOM.render(<Footer />, document.getElementById('footer'));
