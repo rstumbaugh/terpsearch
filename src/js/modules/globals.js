f5f2ed7bbe6f5d98c394eef32adbdd346b6b8edc
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-04-04 12:26:40 -0400

    changed to 8% modal probability

--- src/js/modules/globals.js
+++ src/js/modules/globals.js
@@ -25,7 +25,7 @@ var globals = {
 
 	EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 	
-	MODAL_POPUP_FREQUENCY: 0.05,
+	MODAL_POPUP_FREQUENCY: 0.08,
 
 	getQueryString: function() {
 	    var vars = [], hash;
