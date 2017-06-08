4c93044c197699c28c6fec9cf3ab203a40b78896
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-06-08 18:07:16 -0400

    removed unnecessary files

--- src/scripts/init_firebase.js
+++
@@ -1,3 +0,0 @@
-function loadFirebase() {
-	return $.get(API_CONFIG);
-}
--- src/scripts/load-firebase.js
+++
@@ -1,8 +0,0 @@
-function loadConfig() {
-	return new Promise(function(resolve, reject) {
-		$.get(API_CONFIG, function(data) {
-			firebase.initializeApp(data);
-			resolve();
-		});
-	});
-}
