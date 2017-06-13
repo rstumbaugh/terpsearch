99f27b47a03f4470d8adc3a0f79701ce0d753780
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-06-12 20:20:26 -0400

    added webpack config, removed gulp config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,67 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig =  new HtmlWebpackPlugin({
+  template: __dirname + '/src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
-module.exports = {
+const config = {
   context: path.resolve(__dirname, './src'),
   entry: {
-    admin: './js/admin.js',
-    comments: './js/comments.js',
-    course: './js/course.js',
-    feedback: './js/feedback.js',
-    home: './js/home.js',
-    professor: './js/professor.js',
-    rate: './js/rate.js',
-    search: './js/search.js',
+    app: './js/app.js'
   },
   output: {
-    path: path.resolve(__dirname, './build/src'),
-    filename: '[name].build.js'
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
   },
-}; 
+  resolve: {
+    alias: {
+      styles: path.resolve(__dirname, './src/scss/'),
+      images: path.resolve(__dirname, './src/img/'),
+      components: path.resolve(__dirname, './src/js/components/'),
+      pages: path.resolve(__dirname, './src/js/pages/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['react'] },
+        }],
+      }
+      {
+        test: /\.(sass|scss)$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader',
+          options: {
+            includePaths: ['./scss']
+          }
+        }]
+      },
+      {
+        test: /\.(png|svg|jpe?g)$/,
+        use: 'file-loader'
+      }
+    ],
+  },
+  plugins: [ HtmlWebpackPluginConfig ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+    historyApiFallback: true
+  }
+};
+
+export default config;
