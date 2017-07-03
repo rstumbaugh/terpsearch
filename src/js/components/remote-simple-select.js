bb9f060e5f9dbd29363cb3f9fbbf4d7887d6644f
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-07-03 16:00:26 -0400

    replace react-selectize with react-select

--- src/js/components/remote-simple-select.js
+++ src/js/components/remote-simple-select.js
@@ -1,87 +1,59 @@
 import React, {Component} from 'react';
 import {ReactSelectize, SimpleSelect} from 'react-selectize';
+import Select from 'react-select';
 import Globals from 'globals';
-import * as isofetch from 'isomorphic-fetch';
+import Ajax from 'utils/ajax';
 
+// simple select box from remote data source
 class RemoteSimpleSelect extends Component {
-	constructor(props) {
-		super(props);
-		
-		this.state = {
-			search: '',
-			results: [],
-			value: undefined,
-			minSearchLength: props.minSearchLength || 2
-		}
-	}
-
-	reset() {
+	componentWillReceiveProps(nextProps) {
 		this.setState({
-			search: '',
-			results: [],
-			value: undefined,
-			minSearchLength: this.props.minSearchLength || 2
+			value: nextProps.value || ''
 		})
 	}
 
+	// pass change to parent
+	onChange(value) {
+		this.props.onChange(this.props.name, value.value);
+	}
+
+	// map array of JSON results to array of { label: _, value: _ }
+	getOptions(items) {
+		const textField = this.props.textField;
+		const valueField = this.props.valueField || textField;
+
+    return items.map(i => ({ label: i[textField], value: i[valueField] }));
+  }
+
+  // get options from data source
+	search(input) {
+		if (!input || input.length < (this.props.minSearchLength || 2)) { 
+			return Promise.resolve({ options: [] }) 
+		}
+
+    return Ajax.get(`${this.props.url}${input}`)
+      .then(response => {
+        return JSON.parse(response.response);
+      })
+      .then(response => {
+        const obj = { options: this.getOptions(response) };
+        return obj;
+      })
+      .catch(err => {
+        console.error(err)
+      })
+	}
+
 	render() {
-		var self = this;
-
 		return (
-			<SimpleSelect
-				placeholder={this.props.placeholder}
-				ref='select'
-				theme='bootstrap3'
-				options={this.state.results}
-				search={this.state.search}
-				value={this.state.value}
-
-				onSearchChange={function(search) {
-					self.setState({
-						search: search
-					})
-
-					if (search.length > self.state.minSearchLength) {
-						fetch(self.props.url + search)
-							.then(Globals.handleFetchResponse)
-							.then(function(response) {
-								self.setState({
-									results: response
-								})
-							})
-					}
-				}}
-
-				onValueChange={function(item) {
-					self.setState({
-						value: item
-					})
-					self.props.onValueChange(self.props.name, item[self.props.textField])
-				}}
-
-				filterOptions = {function(options, search){
-	                return options;
-	            }}
-
-				uid={function(item) {
-					return item[self.props.textField]
-				}}
-
-				renderOption={function(item) {
-					return <div className='selectize-option'>{item[self.props.textField]}</div>
-				}}
-
-				renderValue={function(item) {
-					return <div className='selectize-item'>{item[self.props.textField]}</div>
-				}}
-
-				renderNoResultsFound={function(value, search) {
-					return  <div className='selectize-option-empty'>
-								<i>{self.state.search.length > self.state.minSearchLength 
-										? 'No results found' : 'Start typing to search'}</i>
-							</div>
-				}}
-			/>
+			<Select.Async 
+        name={this.props.name}
+        placeholder={this.props.placeholder}
+        loadOptions={this.search.bind(this)}
+        onChange={this.onChange.bind(this)}
+        isLoading={false}
+        value={this.props.value}
+      />
 		)	
 	}
 }
