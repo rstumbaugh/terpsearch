f41e8a7b71fc44885070c60489167f9de1994987
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-08-14 12:23:47 -0400

    created modal to view all friends on profile

--- src/js/components/profile/profile-friends.js
+++ src/js/components/profile/profile-friends.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Modal from 'react-modal';
 import Globals from 'globals';
 
 class ProfileFriends extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			showModal: false
+		}
+	}
+
+	toggleModal(showModal) {
+		this.setState({ showModal });
+	}
+
 	render() {
 		const max = this.props.max || (this.props.friends ? this.props.friends.length : 0)
 		return (
@@ -11,20 +24,38 @@ class ProfileFriends extends Component {
 					{
 						this.props.friends && this.props.friends.length
 							? this.props.friends.slice(0,max).map((friend, i) => 
-									<ProfileFriendItem {...friend} idx={i} key={i} />
+									<ProfileFriendItem {...friend} idx={i} rowSize={3} key={i} />
 								)
 							: <i className='no-friends'>No friends to display.</i>
 					}
 				</div>
 				{
-					this.props.friends && this.props.friends.length > max
-					? <div className='row'>
-							<a href='#'>
-								{`View all friends on TerpSearch (found ${this.props.friends.length})`}
-							</a>
+					this.props.friends && this.props.max && this.props.friends.length > max
+					? <div className='user-profile-view-all-friends' onClick={this.toggleModal.bind(this, true)}>
+							{`View all friends on TerpSearch (found ${this.props.friends.length})`}
 						</div>
 					: ''
 				}
+
+				<Modal
+					isOpen={this.state.showModal}
+					onRequestClose={this.toggleModal.bind(this, false)}
+					contentLabel='Friends modal'
+				>
+					<div className='modal-header'>
+						<h2>
+							Friends on TerpSearch
+							<span className='modal-close' onClick={this.toggleModal.bind(this, false)}>&times;</span>
+						</h2>
+					</div>
+					<div className='user-friends-modal modal-body'>
+						{
+							this.props.friends ? this.props.friends.map((friend, i) => 
+								<ProfileFriendItemRow {...friend} key={i} />
+							) : ''
+						}
+					</div>
+				</Modal>
 			</div>
 		)
 	}
@@ -34,18 +65,35 @@ const ProfileFriendItem = props => {
 	var url = `${Globals.API_USERS}/${props.id}/photo?size=250`;
 	return (
 		<Link to={`/user/${props.id}/`}>
-			<div className='user-profile-friend col-md-4'>
+			<div className={`user-profile-friend col-md-${12 / props.rowSize}`}>
 				<div className='user-profile-friend-img-wrap'>
 					<img src={url} alt={props.name} className='user-profile-friend-img' />
 				</div>
 				<p className='user-profile-friend-name'>{props.name}</p>
 			</div>
 			{
-				(props.idx + 1) % 3 == 0 
+				(props.idx + 1) % props.rowSize == 0 
 					? <div className='clearfix visible-md visible-lg'></div> 
 					: ''
 			}
 		</Link>
+	)
+}
+
+const ProfileFriendItemRow = props => {
+	var url = `${Globals.API_USERS}/${props.id}/photo?size=250`;
+	return (
+		<Link to={`/users/${props.id}`}>
+			<div className='user-friend-row'>
+				<div className='user-friend-row-img-wrap'>
+					<img src={url} className='user-friend-row-img' />
+				</div>
+				<div className='user-friend-row-name'>
+					{props.name}
+				</div>
+			</div>
+		</Link>
+				
 	)
 }
 
